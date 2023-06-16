cd9982dcc60e382a117adccf5fb0f4a5b2544f07
brennan-smith <114622116+brennan-smith@users.noreply.github.com>
2023-06-16 06:28:43 -0700

    enh: DEV-156308 Convert flutter_platform out of a monorepo (#16)

--- lib/property_widgets/widgets/property_4_integer_form.dart
+++ lib/property_widgets/widgets/property_4_integer_form.dart
@@ -1,4 +1,4 @@
-import 'package:alkami_core_dependencies/alkami_core_dependencies.dart';
+import 'package:flutter_platform/alkami_core_dependencies.dart';
 import 'property_dialog.dart';
 
 class FourIntegerForm extends StatefulWidget {
--- lib/property_widgets/widgets/property_dialog.dart
+++ lib/property_widgets/widgets/property_dialog.dart
@@ -1,4 +1,4 @@
-import 'package:alkami_core_dependencies/alkami_core_dependencies.dart';
+import 'package:flutter_platform/alkami_core_dependencies.dart';
 
 class PropertyDialog extends StatelessWidget {
   final String? title;
